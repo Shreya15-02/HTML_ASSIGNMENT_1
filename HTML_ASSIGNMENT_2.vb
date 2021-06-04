575272f1e0bea208ef26d0e185b8eb4a7bec0e2f
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-04 13:18:29 +0530

    Add files via upload

---
+++ HTML_ASSIGNMENT_2.vb
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .ab
+        {
+            font-weight: bold;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        .cd
+        {
+            font-style: italic;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        .ef
+        {
+            text-decoration: underline;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        .gh
+        {
+            font-weight: bold;
+            font-style: italic;
+            margin-top: 0px;
+            margin-bottom: 0px;
+
+        }
+        table,tr,td
+        {
+            border-spacing: 30px 0;
+            
+        }
+        div
+        {
+            text-align: left;
+        }
+        }
+    </style>
+</head>
+<body>
+    <div>
+    <h1 style="font-weight: bold;">Patrick Young</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quisquam, ab expedita illum labore qui necessitatibus eius mollitia sequi magnam dolore cumque sit quasi possimus eos dignissimos, numquam eaque iste ex. Accusamus, numquam fugit. Ducimus iste adipisci aperiam? Facere distinctio, deserunt doloremque laborum eos pariatur fugiat nulla unde, impedit in corporis. Sint temporibus praesentium quo sequi harum vel soluta deleniti? ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt reprehenderit nostrum quibusdam inventore earum id obcaecati, rerum ipsam officia quas et consectetur quasi sint provident minima, vel fugiat, dicta dolorem. Debitis aperiam praesentium, officia ex iure sed sint nobis nulla, nisi reprehenderit itaque, iste quas explicabo modi ducimus doloremque recusandae.</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nesciunt mollitia! Rerum vitae accusantium rem aut facilis. Dignissimos hic rem ratione inventore quia. Sint molestiae vitae nam possimus qui rem earum enim odit veritatis. Ad natus fugit quos libero explicabo provident molestiae distinctio placeat impedit molestias, iste nesciunt itaque fuga?</p>
+     <p class="ab">This text is bold</p>
+     <p class="cd">While this is in Italic</p>
+     <p class="ef">and this is underlined</p>
+     <p class="gh">Look at this, this is bold and italic</p>
+     <table>
+         <tr>
+             <td>C<sub>2</sub>H<sub>3</sub>O<sub>2</sub><sup>-</sup></td>
+             <td>acetate</td>
+         </tr>
+         <tr>
+            <td>MnO<sub>4</sub><sup>-</sup></td>
+            <td>permanganate</td>
+        </tr>
+        <tr>
+            <td>Cr<sub>2</sub>O<sub>7</sub><sup>2-</sup></td>
+            <td>dichromate</td>
+        </tr>
+        <tr>
+            <td>CrO<sub>4</sub><sup>2-</sup></td>
+            <td>chromate</td>
+        </tr>
+        <tr>
+            <td>O<sub>2</sub><sup>2-</sup></td>
+            <td>peroxide</td>
+        </tr>
+     </table>
+     <p>(x+y)<sup>2</sup> = x<sup>2</sup>+2xy+y<sup>2</sup></p>
+     <p>(x-y)<sup>2</sup> = x<sup>2</sup>-2xy+y<sup>2</sup></p>
+     <p>x<sup>2</sup>-y<sup>2</sup> = (x-y)(x+y)</p>
+     <p>(x+a)(x+b) = x<sup>2</sup>+(a+b)x+ab</p>
+    </div>
+     
+</body>
+</html>
