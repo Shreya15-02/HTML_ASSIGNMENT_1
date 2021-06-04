d7da486c835b7cfb1d88917b01010c7d56ff448b
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-04 13:20:24 +0530

    Add files via upload

---
+++ HTML_ASSIGNMENT_3.vb
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table,tr,td
+        {
+            border-spacing:30px;
+        }
+        .ab
+        {
+            background-color: whitesmoke;
+        }
+    </style>
+</head>
+<body>
+    <div class="ab">
+    <table>
+        <tr>
+            <td>
+                <h2>Standford researchers find a quicker cheaper way to sort istopoes</h2>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora autem, quas libero eveniet odit, vero, repellendus incidunt sit architecto suscipit corporis doloremque quod molestias nostrum eaque saepe. Ullam ea facilis nemo? Dolores odio doloremque dolor velit tempora accusantium nostrum nam.</p>
+            </td>
+            <td>
+                <h2>Chance to dance :Enthusiasts fly to Standford for dance weekend</h2>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam maiores maxime quis et! Porro, tempore facilis quibusdam optio ipsa voluptates veniam. Quaerat velit autem unde sed quas enim quae libero labore quo impedit iure aliquam, doloremque sunt odit esse iusto.</p>
+            </td>
+        </tr>
+    </table>
+</div>
+        </html>
