a5028e4d0839b24f838eef69282ea52e58e99808
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-04 12:53:45 +0530

    Add files via upload

---
+++ HTML_ASSIGNMENT_1.vb
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *
+        {
+            box-sizing: border-box;
+        }
+        h1
+        {
+            text-align: center;
+            font-weight: bold;
+            
+        }
+        h3{
+            text-align: center;
+             font-style:italic;
+             margin-bottom: 0px;
+             margin-top: 0px;
+             
+        }
+        h2
+        {
+            text-align: center;
+            font-weight:500;
+        }
+        h4
+        {
+            font-weight: bold;
+            text-align:left;
+        }
+        #ab
+        {
+            text-decoration: underline;
+            font-style: italic;
+        }
+        
+    </style>
+</head>
+<body>
+    <h1>The Great Web Page Of</h1>
+    <h3>John Q.Student</h3>
+    <h3>#1212</h3>
+    <h3>Section 001(MWF 8:30 am)</h3>
+    <h3>Assignment#2</h3>
+    <h2>My Summer Adventures</h2>
+    <section >
+        <h4>My Trip To California</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus modi ea debitis assumenda. Veritatis, voluptate delectus possimus aut, eaque et unde quidem in non nesciunt commodi consequatur ea saepe illum praesentium assumenda perspiciatis ad dignissimos veniam iste, cum ab. Praesentium, provident necessitatibus distinctio deleniti nam iste ratione molestiae officia aliquid nobis eligendi dicta. Quidem omnis rerum temporibus commodi corrupti nihil.</p>
+    </section>
+    <section >
+        <h4>My Trip To The Rockies</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia placeat est expedita deleniti nostrum unde! Voluptatibus vitae hic reiciendis. Sit qui delectus eveniet eos dolor animi amet quidem porro possimus a repellat suscipit error cum culpa neque accusantium excepturi, facilis earum sint placeat ipsam reprehenderit libero! Itaque ducimus ipsam at. ipsum dolor sit amet consectetur adipisicing elit. Reiciendis error aperiam officia minus quae illo cupiditate quibusdam ducimus a eum animi vero consequuntur, ab dolores dolorum laboriosam blanditiis hic. Eum repellendus tempora velit est beatae perspiciatis provident commodi magni, ipsam temporibus soluta illo ab iusto saepe repudiandae fugit corporis expedita repellat in iure quo dolores. Enim autem rerum impedit suscipit?</p>
+    </section>
+    <section>
+        <h4>My Trip To The Mid west</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati minus inventore, a eveniet amet provident excepturi, voluptates reprehenderit architecto et hic sed ad neque illum doloribus facilis quod, magnam animi mollitia voluptate adipisci perspiciatis quam? Odio vero aliquam soluta nisi itaque. Illum autem consequuntur, omnis, nostrum ipsam ad, praesentium corporis dignissimos quisquam quasi rem itaque nisi quas vero reprehenderit nesciunt.</p>
+    </section>
+    <h4>Concluding Thought About The Summer</h4>
+    <p id="ab">WHAT A SUMMER! <span> If only they could all be this great!</span></p>
+</body>
+</html>
